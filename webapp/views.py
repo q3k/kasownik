f33d011a705bb6bd5770566c17493982f3eb7b4e
Sergiusz 'q3k' Bazański <q3k@q3k.org>
2013-09-25 13:40:51 +0200

    Better email spam.

--- webapp/views.py
+++ webapp/views.py
@@ -183,19 +183,22 @@ def match_user_transfer():
 @login_required
 def sendspam():
     spam = []
-    members = models.Member.query.all()
+    members = models.Member.query.filter_by(active=True).all()
     for member in members:
-        details = "\n".join([" - opłata za %02i/%i, pokryta przelewem za %.2f PLN w dniu %s" \
-            % (mt.month, mt.year, mt.transfer.amount/100,  mt.transfer.date.strftime("%d/%m/%Y")) for mt in member.transfers])
+    	transfers = sorted(member.transfers, key=lambda mt: mt.year * 12 + (mt.month-1) )
+        details = u"\n".join([u" - opłata za %02i/%i, pokryta przelewem za %.2f PLN w dniu %s" \
+            % (mt.month, mt.year, mt.transfer.amount/100,  mt.transfer.date.strftime("%d/%m/%Y")) for mt in transfers])
         months_due = member.months_due()
-        money_due = months_due * 100 if member.type == 'fatty' else months_due * 50
+        money_due = months_due * 10000 if member.type == 'fatty' else months_due * 5000
         due = "???"
         if months_due > 0:
             due = u"Jesteś %i składek (%i PLN) do tyłu. Kiepsko." % (months_due, money_due/100)
+            if months_due < 5:
+                due = u"Jesteś %i składki (%i PLN) do tyłu. Kiepsko." % (months_due, money_due/100)
             if months_due == 1:
-                due = u"Jesteś o składkę (%i PLN) do tyłu." % money_due/100
+                due = u"Jesteś o składkę (%i PLN) do tyłu." % (money_due/100)
             if months_due > 2:
-                due += u"""\nZgodnie z regulaminem HS, trzymiesięczne zaległość w składkach oznacza automatyczne wykreślenie z listy członków i usunięcie karty z zamka.
+                due += u"""\nZgodnie z regulaminem HS, trzymiesięczna zaległość w składkach oznacza automatyczne wykreślenie z listy członków i usunięcie karty z zamka.
 Masz tydzień na uregulowanie składki od daty wysłania tego emaila."""
         elif months_due == 0:
             due = u"Jesteś na bieżąco ze składkami. Hura!"
@@ -236,6 +239,9 @@ Hackerspace'owy Faszysta
         spam.append(msg)
 
     for msg in spam:
+    	#f = open("/tmp/spamspamspam", "a")
+	#f.write(msg.as_string())
+	#f.close()
         p = Popen(["/usr/sbin/sendmail", "-t"], stdin=PIPE)
         p.communicate(msg.as_string())
     return "done!"
